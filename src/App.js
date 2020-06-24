8311fb1c21ff3de1c87790fbcc941afc47c327fb
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 10:26:45 -0300

    moved router from appjs to indexjs

--- src/App.js
+++ src/App.js
@@ -16,29 +16,23 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/" component={Curriculum} />
-          <Route exact path="/journals" component={Journals} />
-          <Route exact path="/conferencias" component={Conferencias} />
-          <Route exact path="/downloads" component={Downloads} />
-          <Route exact path="/livros" component={Livros} />
-          <Route exact path="/parceiros" component={Parceiros} />
-          <Route exact path="/alunos" component={Alunos} />
-          <Route
-            exact
-            path="/reforco-estrutural"
-            component={ReforcoEstrutural}
-          />
-          <Route exact path="/patologia" component={Patologia} />
-          <Route
-            exact
-            path="/sistemas-estruturais"
-            component={SistemasEstruturais}
-          />
-        </Switch>
-      </Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Curriculum} />
+        <Route exact path="/journals" component={Journals} />
+        <Route exact path="/conferencias" component={Conferencias} />
+        <Route exact path="/downloads" component={Downloads} />
+        <Route exact path="/livros" component={Livros} />
+        <Route exact path="/parceiros" component={Parceiros} />
+        <Route exact path="/alunos" component={Alunos} />
+        <Route exact path="/reforco-estrutural" component={ReforcoEstrutural} />
+        <Route exact path="/patologia" component={Patologia} />
+        <Route
+          exact
+          path="/sistemas-estruturais"
+          component={SistemasEstruturais}
+        />
+      </Switch>
     </>
   );
 }
