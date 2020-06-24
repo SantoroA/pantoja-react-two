1dc4ab3ef7a8f4ce6a20d51b8ac16c9b90b4af6c
SantoroA <64786843+SantoroA@users.noreply.github.com>
2020-06-24 00:23:38 -0300

    changed router from index to app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./Navbar";
 import Curriculum from "./Curriculum";
 import Journals from "./Journals";
@@ -16,23 +16,29 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Curriculum} />
-        <Route exact path="/journals" component={Journals} />
-        <Route exact path="/conferencias" component={Conferencias} />
-        <Route exact path="/downloads" component={Downloads} />
-        <Route exact path="/livros" component={Livros} />
-        <Route exact path="/parceiros" component={Parceiros} />
-        <Route exact path="/alunos" component={Alunos} />
-        <Route exact path="/reforco-estrutural" component={ReforcoEstrutural} />
-        <Route exact path="/patologia" component={Patologia} />
-        <Route
-          exact
-          path="/sistemas-estruturais"
-          component={SistemasEstruturais}
-        />
-      </Switch>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Curriculum} />
+          <Route exact path="/journals" component={Journals} />
+          <Route exact path="/conferencias" component={Conferencias} />
+          <Route exact path="/downloads" component={Downloads} />
+          <Route exact path="/livros" component={Livros} />
+          <Route exact path="/parceiros" component={Parceiros} />
+          <Route exact path="/alunos" component={Alunos} />
+          <Route
+            exact
+            path="/reforco-estrutural"
+            component={ReforcoEstrutural}
+          />
+          <Route exact path="/patologia" component={Patologia} />
+          <Route
+            exact
+            path="/sistemas-estruturais"
+            component={SistemasEstruturais}
+          />
+        </Switch>
+      </Router>
     </>
   );
 }
